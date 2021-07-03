79e8041b1da403f4c7a5bd50c9cc7514003666b0
João Walter Amadeu <joaowa2002@live.com>
2021-07-02 22:41:46 -0300

    Resolvido problema com os espaços nos cantos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import ListaComponent from './components/lista';
 import Seletor from './components/seletor';
 import { useSelector } from "react-redux"
+import preval from 'preval.macro'
 function App() {
   const result = useSelector(state => state.Seletor)
   return (
-    <div className={`${result['tc']} ${result['bg']} container`} >
+    <div className={`${result['tc']} ${result['bg']}`} style={{width:"100%"}} >
       <div className="container">
-        <div className="mt-5 ms-4" >
+        <div className="mt-5" >
           <h3 >
             Lista de coisas à fazer
           </h3>
           <Seletor></Seletor>
           <ListaComponent></ListaComponent>
         </div>
+        <p className="mt-5">
+            Data de build: {preval`module.exports = new Date().toLocaleString();`}.
+          </p>
       </div>
+      
     </div>
   );
 }
